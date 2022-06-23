9e8e4fff7bb6be6cae9bce0a62d435ceea64e49b
on3s228 <kryashow@yandex.ru>
2022-06-23 14:00:46 +0300

    dotenv changes

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use Dotenv\Dotenv;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv = Dotenv::createImmutable(__DIR__);
 define("env", $dotenv->load());
 
 
