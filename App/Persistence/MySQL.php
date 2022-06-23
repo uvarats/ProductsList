5df5ed7a5c3604f9349846d94d0971fb807b25ea
on3s228 <kryashow@yandex.ru>
2022-06-23 14:51:09 +0300

    fixed really laughy mistake

--- App/Persistence/MySQL.php
+++ App/Persistence/MySQL.php
@@ -13,7 +13,7 @@ class MySQL
     public function __construct()
     {
         $this->connection = new mysqli(
-            $_ENV('DB_HOST'),
+            $_ENV['DB_HOST'],
             $_ENV['DB_USER'],
             $_ENV['DB_PASSWORD'],
             $_ENV['DB_DATABASE']
